f509eca743e06a59e1255cbba8849eb563130a62
shivanshious <shivanshgoel98@gmail.com>
2022-10-12 15:52:21 +0530

    commit on 12 oct

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./weather.css";
-import AcUnitIcon from '@mui/icons-material/AcUnit';
+import AcUnitIcon from "@mui/icons-material/AcUnit";
 
 const data = {
   key: "3668d161d7faa360f4c291f46426e526",
@@ -14,10 +14,15 @@ var requestOptions = {
 
 const Weather = () => {
   const [weatherInfo, setWetherInfo] = useState();
-  const [city, setCity] = useState("Delhi");
+  const [city, setCity] = useState("");
 
   const handleChange = (event) => {
     setCity(event.target.value);
+  };
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      findData();
+    }
   };
 
   const findData = () => {
@@ -28,7 +33,8 @@ const Weather = () => {
       .then((response) => response.text())
       .then((result) => {
         setWetherInfo(JSON.parse(result));
-        console.log(weatherInfo.weather[0].main);
+        setCity("");
+        // console.log(weatherInfo);
       })
       .catch((error) => console.log("error", error));
   };
@@ -40,10 +46,11 @@ const Weather = () => {
           <input
             type="text"
             className="input"
-            // value={city}/
+            value={city}
             onChange={handleChange}
+            onKeyUp={handleKeyPress}
           />
-          <button className="search_button" onClick={findData}>
+          <button type="submit" className="search_button" onClick={findData}>
             Search
           </button>
         </div>
@@ -51,38 +58,50 @@ const Weather = () => {
           <div className="content">
             <div className="display_name">
               <h1 className="city_name">
-                {city.charAt(0).toUpperCase() + city.slice(1)}
+                {/* {weatherInfo.name.charAt(0).toUpperCase() + weatherInfo.name.slice(1)} */}
+                {weatherInfo.name}
               </h1>
               <h3 className="weather_main">{weatherInfo.weather[0].main}</h3>
             </div>
             <div className="other_info">
               <div className="item_holder">
-
-              <img
-                src={`http://openweathermap.org/img/w/${weatherInfo.weather[0].icon}.png`}
-                alt="icon"
-                className="icon"
+                <img
+                  src={`http://openweathermap.org/img/w/${weatherInfo.weather[0].icon}.png`}
+                  alt="icon"
+                  className="icon"
                 />
-              <p className="description">
-                {weatherInfo.weather[0].description}
-              </p>
+                <p className="description">
+                  {weatherInfo.weather[0].description}
+                </p>
+              </div>
+              <div className="item_holder">
+                <h2 className="heading">{weatherInfo.main.temp} &deg;C</h2>
+                <p className="feel_like">
+                  Feel Like: {weatherInfo.main.feels_like} &deg;C
+                </p>
+                <div className="min_max_temp">
+                  <p className="min_temp">
+                    min: {weatherInfo.main.temp_min} &deg;C
+                  </p>
+                  <p className="max_temp">
+                    max: {weatherInfo.main.temp_max} &deg;C
+                  </p>
                 </div>
-            <div className="item_holder"> 
-            <h2 className="heading">{weatherInfo.main.temp} &deg;C</h2>
-            <p className="feel_like">Feel Like: {weatherInfo.main.feels_like} &deg;C</p>
-            <div className="min_max_temp">
-              <p className="min_temp">min: {weatherInfo.main.temp_min} &deg;C</p>
-              <p className="max_temp">max: {weatherInfo.main.temp_max} &deg;C</p>
+              </div>
+              <div className="item_holder">
+                <AcUnitIcon className="wind_icon" />
+                <p className="wind">Speed: {weatherInfo.wind.speed} km/h</p>
+                <p className="wind">Direction: {weatherInfo.wind.deg} &deg;</p>
+              </div>
             </div>
-              
-            </div>
-            <div className="item_holder">
-              <AcUnitIcon className="wind_icon"/>
-             
-              <p className="wind">Speed: {weatherInfo.wind.speed} km/h</p>
-              <p className="wind">Direction: {weatherInfo.wind.deg} &deg;C</p>
- 
-            </div>
+            <div className="extras">
+              <p className="extra_item">
+                Pressure: {weatherInfo.main.pressure}
+              </p>
+              <p className="extra_item">
+                Humidity: {weatherInfo.main.humidity}
+              </p>
+              <p className="extra_item">Visibility: {weatherInfo.visibility}</p>
             </div>
           </div>
         ) : (
